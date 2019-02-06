acef11e2eb4b2858f467023a17fd282237b3275e
Sofia Jonsson <jonssonsofia93@gmail.com>
2019-02-06 04:30:53 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,23 @@
-def oxford_comma(string)
-  string.join(" and ") 
-  
-end
+# def oxford_comma(array)
+#   if array.length == 1
+#     return "#{array[0]}"
+#   elsif array.length == 2
+#     return array.join(" and ")
+#   elsif array.length >= 3
+#     array[-1] = "and #{array[-1]}"
+#     return array.join(", ")
+#   end
+# end
 
+def oxford_comma(array)
+  if array.length == 1
+    return "#{array[0]}"
+  elsif array.length == 2
+    return array.join(" and ")
+  elsif array.length >= 3
+    array2 = "and #{array[-1]}"
+    array.pop
+    array.push(array2)
+    return array.join(", ")
+  end
+end
