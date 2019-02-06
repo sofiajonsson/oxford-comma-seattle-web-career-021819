48a9fe2c20661166f4f0f3a57d314df44bdf5783
Sofia Jonsson <jonssonsofia93@gmail.com>
2019-02-06 04:31:14 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,10 @@
-def oxford_comma(string)
-  string.join(" and ") 
-  
+def oxford_comma(array)
+  if array.length == 1
+    return "#{array[0]}"
+  elsif array.length == 2
+    return array.join(" and ")
+  elsif array.length >= 3
+    array[-1] = "and #{array[-1]}"
+    return array.join(", ")
+  end
 end
-
